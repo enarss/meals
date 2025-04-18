8bce201b71a12249cb1f6f234dbea727f09da1b2
enarss <enarss@outlook.com>
2025-04-18 13:34:01 +0330

    add like Animated favorite icon

--- src/features/recipe/components/RecipeCard.tsx
+++ src/features/recipe/components/RecipeCard.tsx
@@ -3,6 +3,7 @@ import {
   CardActionArea,
   CardContent,
   CardMedia,
+  IconButton,
   Typography,
 } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
@@ -10,6 +11,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../store/rootReducer";
 import { Meal } from "../slices/recipeSlice";
 import { toggleFavoriteAction } from "../slices/favoritesSlice";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 interface Props {
   meal: Meal;
@@ -21,8 +24,12 @@ const RecipeCard = ({ meal, onClick }: Props) => {
     state.favorites.list.some((item) => item.idMeal === meal.idMeal)
   );
   const dispatch = useDispatch();
-  const handleToggleFavorite = () => {
+  const [clicked, setClicked] = useState(false);
+  const handleToggle = (e: React.MouseEvent) => {
     dispatch(toggleFavoriteAction(meal));
+    e.stopPropagation();
+    setClicked(true);
+    setTimeout(() => setClicked(false), 400);
   };
   const iconColor = isFavorite ? "error" : "disabled";
 
@@ -34,13 +41,25 @@ const RecipeCard = ({ meal, onClick }: Props) => {
           <Typography gutterBottom variant="h5" component="div">
             {meal.strMeal}
           </Typography>
-          <FavoriteIcon
-            onClick={(e) => {
-              e.stopPropagation();
-              handleToggleFavorite();
-            }}
-            color={iconColor}
-          />
+          <IconButton size="medium" onClick={(e) => handleToggle(e)}>
+            <motion.div
+              key={isFavorite ? "liked" : "unliked"}
+              animate={clicked ? { scale: [1, 1.4, 1], opacity: [1, 0.9, 1] } : {}}
+              transition={{ duration: 0.4, ease: "easeInOut" }}
+            >
+              <motion.div
+                animate={isFavorite ? { scale: [1, 1.1, 1] } : {}}
+                transition={{
+                  repeat: Infinity,
+                  duration: 1.5,
+                  repeatType: "reverse",
+                  ease: "easeInOut",
+                }}
+              >
+                <FavoriteIcon color={iconColor} />
+              </motion.div>
+            </motion.div>
+          </IconButton>
         </CardContent>
       </CardActionArea>
     </Card>
